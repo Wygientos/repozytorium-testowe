a35b5c2b36029190c6e85322e58d0f4f40796574
TheMyster1234 <maciej.rzymek@gmail.com>
2020-12-22 15:05:10 +0100

    Update main3.py

--- main3.py
+++ main3.py
@@ -1,4 +1,8 @@
 a = int ( input ( ' Podaj pierwsza liczbe: ') )
-b = = int ( input ( ' Podaj druga liczbe : ') )
+b = int ( input ( ' Podaj druga liczbe : ') )
+def nwd(a, b):
+    if b > 0:
+        return nwd(b, a%b)
+    return a
 def nww(a, b): return a*b//nwd(a, b)
 print 
